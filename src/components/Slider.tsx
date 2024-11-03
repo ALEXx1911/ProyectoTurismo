2278cdddf6bee762585e687c848dd34742913336
alex <ac8369745@gmail.com>
2024-11-03 18:26:55 +0100

    Implementación de un método UseEffect , pues si se usa un intervalo solo, el intervalo incrementa la rapidez del cambio y falla, por lo que en el UseEffect se usa un clearInterval para limpiar el intervalo

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SliderButton from "./Slider-Button";
 
 export default function Slider(){
@@ -15,6 +15,10 @@ export default function Slider(){
         const nextIndex= selectedIndex <= 0 ? imagenes.length-1 : selectedIndex-1;
         setSelectedIndex(nextIndex);
     }
+    useEffect(()=>{
+        const intervalo = setInterval(moveToRight,5000);
+        return () => clearInterval(intervalo);
+    });
 
     return(
         <>
