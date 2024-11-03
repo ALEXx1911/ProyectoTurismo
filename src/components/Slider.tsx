33249e1bcd61a792a28252a18d933287a7ac8d0f
alex <ac8369745@gmail.com>
2024-11-03 17:51:14 +0100

    Creado un array para guardar las url de las imagenes y un estado para poder cambiar las imagenes. También cambiado el nombre de una imagén

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import SliderButton from "./Slider-Button";
 
 export default function Slider(){
+    const urlImg='../../img/';
+    const imagenes: string []=[urlImg+'Palacio-Real-de-Madrid.jpeg',urlImg+'Real-Alcaraz-de-Sevilla.jpeg'];
+    const [selectedIndex, setSelectedIndex]= useState(0);
     return(
         <>
         <section className="slider-carousel">
         <section className="item-list">
-            <img src="" alt="xd" className="img-ite"/>
+            <img src={imagenes[selectedIndex]} alt="xd" className="img-ite"/>
         </section>
         </section>
         <SliderButton />
