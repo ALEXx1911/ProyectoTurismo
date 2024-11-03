15a71c2be2eb15e31fd4fc1fb1028cad8696183a
alex <ac8369745@gmail.com>
2024-11-03 17:36:31 +0100

    Creacion de la estructura HTML del Slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,14 @@
+import SliderButton from "./Slider-Button";
+
 export default function Slider(){
     return(
         <>
-            
+        <section className="slider-carousel">
+        <section className="item-list">
+            <img src="" alt="xd" className="img-ite"/>
+        </section>
+        </section>
+        <SliderButton />
         </>
     );
 }
