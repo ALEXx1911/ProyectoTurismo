3c21223a03d3b31f1ad37e011c2632b5b1068717
alex <ac8369745@gmail.com>
2024-11-03 18:05:28 +0100

    Creacion de las funciones para cambiar las imagenes al pulsar los botones

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,6 +5,17 @@ export default function Slider(){
     const urlImg='../../img/';
     const imagenes: string []=[urlImg+'Palacio-Real-de-Madrid.jpeg',urlImg+'Real-Alcaraz-de-Sevilla.jpeg'];
     const [selectedIndex, setSelectedIndex]= useState(0);
+
+    function moveToRight(){
+        const nextIndex= selectedIndex >= imagenes.length-1 ? 0 : selectedIndex+1;
+        setSelectedIndex(nextIndex);
+    }
+
+    function moveToLeft(){
+        const nextIndex= selectedIndex < 0 ? imagenes.length-1 : selectedIndex+1;
+        setSelectedIndex(nextIndex);
+    }
+
     return(
         <>
         <section className="slider-carousel">
