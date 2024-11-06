8615f1ee8a80a67b592f1a6153b2fae2cde1193e
alex <ac8369745@gmail.com>
2024-11-06 10:06:23 +0100

    Se añade las etiquetas svg al componente SliderButton

--- src/components/Slider-Button.tsx
+++ src/components/Slider-Button.tsx
@@ -5,8 +5,12 @@ type SliderButtonProps={
 export default function SliderButton({functionLeft, functionRight}: SliderButtonProps){
     return(
         <>
-        <div className="slider-btn button-left" onClick={functionLeft}>{"<"}</div>
-        <div className="slider-btn button-right" onClick={functionRight}>{">"}</div>
+        <div className="slider-btn button-left" onClick={functionLeft}>
+        <svg xmlns="http://www.w3.org/2000/svg" id="Bold" viewBox="0 0 24 24" width="512" height="512"><path d="M17.921,1.505a1.5,1.5,0,0,1-.44,1.06L9.809,10.237a2.5,2.5,0,0,0,0,3.536l7.662,7.662a1.5,1.5,0,0,1-2.121,2.121L7.688,15.9a5.506,5.506,0,0,1,0-7.779L15.36.444a1.5,1.5,0,0,1,2.561,1.061Z"/></svg>
+        </div>
+        <div className="slider-btn button-right" onClick={functionRight}>
+        <svg xmlns="http://www.w3.org/2000/svg" id="Bold" viewBox="0 0 24 24" width="512" height="512"><path d="M6.079,22.5a1.5,1.5,0,0,1,.44-1.06l7.672-7.672a2.5,2.5,0,0,0,0-3.536L6.529,2.565A1.5,1.5,0,0,1,8.65.444l7.662,7.661a5.506,5.506,0,0,1,0,7.779L8.64,23.556A1.5,1.5,0,0,1,6.079,22.5Z"/></svg>
+        </div>
         </>
     );
 }
