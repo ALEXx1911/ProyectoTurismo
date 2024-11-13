0e52cb8422d36ca6c0ad7170127b0de77bdc664b
Ninjawarsgamine <robertomr555@gmail.com>
2024-11-13 09:45:07 +0100

    Se ha añadido un componente Search_bar.tsx. para la barra de búsqueda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,25 @@
-import { useContext, useState } from "react";
+import { useContext, useState,createContext } from "react";
 import ProfileBox from "./ProfileBox";
 import Menu from "./Menu";
 import { AppContext } from "../App";
-
+import Search_bar from "./Search-bar";
+type HeaderContext = {
+    showList:boolean;
+    setShowList:(x:boolean)=>void;
+  };
+  
+  export const HeaderContext = createContext<HeaderContext>({
+    showList:false,
+    setShowList: () => {},
+  });
 export default function Header(){
     const [showMenu,setShowMenu]=useState(false);
     //Variable para controlar el estado de visibilidad de "Menu".
 
     const {username, showLogin,setShowLogin}=useContext(AppContext);
     //Utilizando un contexto, tomamos los valores de "username","showLogin" y "setShowLogin" de "App" para usarlos en "Header".
+
+    const [showList,setShowList]=useState(false);
     
     function handleClickProfileBox(){
        showMenu?setShowMenu(false):setShowMenu(true);
@@ -21,7 +32,9 @@ export default function Header(){
     }
     //Función para activar el formulario de login cuando se pulsa en "Iniciar sesión".
     //Además, también desactiva el componente "Menu".
-
+    function handleClickSearchbar(){
+        showList===true?setShowList(false):setShowList(true);
+    }
     return(
         <>
         <div className="header">
@@ -29,13 +42,9 @@ export default function Header(){
                <img className=" header__logo"src="../../img/logo.jpeg" alt="Foto del logo de la compañía"/>
                <h4 className="header__name">turismoEspaña</h4>
             </div>
-       
-                <form className="header__search-bar">
-                    <input type="text" className="header__search-bar__input" placeholder="Buscar lugares"/>
-                    <button className="header__search-bar__search-button">
-                    <i className="header__icon icon-basic-magnifier"></i>
-                    </button> 
-                </form>
+                <HeaderContext.Provider value={{showList,setShowList}}>
+                <Search_bar handleClickSearchBar={handleClickSearchbar}/>
+                </HeaderContext.Provider>
             <div className="header__profile-box">
                 <ProfileBox image="../../img/imagen-perfil-default.jpg" username={username} 
                 handleClickProfileBox={handleClickProfileBox}/>
