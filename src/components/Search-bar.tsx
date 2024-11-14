219379cb9be33412aade0aa12f4d13a519f44d2d
Ninjawarsgamine <robertomr555@gmail.com>
2024-11-14 09:29:01 +0100

    Estilo de la barra arreglado.

--- src/components/Search-bar.tsx
+++ src/components/Search-bar.tsx
@@ -9,11 +9,12 @@ export default function Search_bar({handleClickSearchBar}:Search_barProps){
     return(
     <form className="header__search-bar">
         <div className="header__search-bar-container">
-        <input type="text" className="header__search-bar__input" placeholder="Buscar lugares" onClick={handleClickSearchBar}/>
-        <button className="header__search-bar__search-button">
+        <input type="text" className="header__search-bar-container__input" placeholder="Buscar lugares" onClick={handleClickSearchBar}/>
+        <button className="header__search-bar-container__search-button">
         <i className="header__icon icon-basic-magnifier"></i>
         </button> 
         </div>
+        
        {/* Aquí hay que meter el "Provincia.tsx" y al hacerle click en una de las opciones, que se ponga el texto en el "input".
         Al darle a buscar, que acceda a la página de la provincia especificada. */}
 
