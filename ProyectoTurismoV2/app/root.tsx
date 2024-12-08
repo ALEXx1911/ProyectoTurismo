1a4a846585b548f383dd21d27b062166ad17cefd
Ninjawarsgamine <robertomr555@gmail.com>
2024-12-08 14:21:19 +0100

    Se ha añadido el favicon para la página, se ha añadido la opción de Provincias Favoritas al menú y se han hecho algunos cambios menores

--- ProyectoTurismoV2/app/root.tsx
+++ ProyectoTurismoV2/app/root.tsx
@@ -22,6 +22,11 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel:"shortcut icon",
+    href:"img/image-favicon.png",
+    type:"image/x-icon"
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
