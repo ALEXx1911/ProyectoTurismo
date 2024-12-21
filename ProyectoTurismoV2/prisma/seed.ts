9c2ec69c2a34a44ab086ec5382978b679deff658
Ninjawarsgamine <robertomr555@gmail.com>
2024-12-21 19:05:33 +0100

    Cambios menores

--- ProyectoTurismoV2/prisma/seed.ts
+++ ProyectoTurismoV2/prisma/seed.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { hashPassword } from "~/utils/utils";
 //import { data } from "@remix-run/react";
 const db = new PrismaClient();
 
 function crearUser(){
-    const password="ejemplo";
-    const passwordHashed=hashPassword(password);
     return db.user.create({
         data:{
             email:"ejemplo@gmail.com",
