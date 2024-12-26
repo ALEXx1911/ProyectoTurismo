20711121e46c708a1226ae8b50802a9a6d443032
alex <ac8369745@gmail.com>
2024-12-26 23:24:02 +0100

    se cambian los títulos de las imágenes del slider (son provisionales), para que sean diferentes, también se cambia el color del tema de swiper

--- ProyectoTurismoV2/app/components/Slider.tsx
+++ ProyectoTurismoV2/app/components/Slider.tsx
@@ -51,35 +51,37 @@ export default function Slider(){
             spaceBetween={30}
             >
             <SwiperSlide>
-                <SliderItem imgUrl={imagenes[0]}></SliderItem>
+                <SliderItem imgUrl={imagenes[0]} titulo="Descubre España con Nosotros" subtitulo="Innumerables paisajes"></SliderItem>
             </SwiperSlide>
             <SwiperSlide>
-                <SliderItem imgUrl={imagenes[1]}></SliderItem>
+                <SliderItem imgUrl={imagenes[1]} titulo="La esencia de la naturaleza" subtitulo="Biodiversidad única"></SliderItem>
             </SwiperSlide>
             <SwiperSlide>
-                <SliderItem imgUrl={imagenes[2]}></SliderItem>
+                <SliderItem imgUrl={imagenes[2]} titulo="Vibrantes ciudades nocturnas" subtitulo="Actividad incluso de noche"></SliderItem>
             </SwiperSlide>
             <SwiperSlide>
-                <SliderItem imgUrl={imagenes[3]}></SliderItem>
+                <SliderItem imgUrl={imagenes[3]} titulo="Paraíso frente al mar" subtitulo="Maravillosas playas"></SliderItem>
             </SwiperSlide>
             <SwiperSlide>
-                <SliderItem imgUrl={imagenes[4]}></SliderItem>
+                <SliderItem imgUrl={imagenes[4]} titulo="Historia y cultura en cada paso" subtitulo="Explora sitios históricos"></SliderItem>
             </SwiperSlide>
         </Swiper>
     );
 }
 type SliderItemProps = {
     imgUrl : string,
+    titulo: string,
+    subtitulo:string,
 }
-function SliderItem({imgUrl}:SliderItemProps){
+function SliderItem({imgUrl,titulo,subtitulo}:SliderItemProps){
     return(
         <div className="w-full h-full overflow-hidden m-auto">
             <img src={imgUrl} alt="xd" className="w-full h-full object-cover"/>
             <div className={classNames("flex flex-col items-center absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2",
                 "text-white text-center text-2xl font-semibold md:text-4xl"
             )}>
-                <span className="text-base">Innumerables paisajes</span>
-                <span className="mainTitle">Descubre España con Nosotros</span>
+                <span className="text-base">{subtitulo}</span>
+                <span className="mainTitle">{titulo}</span>
                 <button className={classNames(
                     "text-xl w-32 h-12 mt-4 translate-y-0 bg-[rgba(0,0,0,0.35)]",
                     "hvr-sweep-to-top"
