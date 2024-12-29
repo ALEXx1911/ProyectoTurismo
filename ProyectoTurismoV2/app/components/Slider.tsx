b94f67f76e35a6ab191520a5925bdedf1dc0a396
alex <ac8369745@gmail.com>
2024-12-29 17:33:48 +0100

    Solucionado el problema de las vibraciones en el slider, el problema ocurria por no poner en false la propiedad resizeObserver, que creaba un bucle de redimensionamiento al cambiar el tamaño de la pantalla, ahora el redimensionamiento lo haze swiper y no hay vibraciones

--- ProyectoTurismoV2/app/components/Slider.tsx
+++ ProyectoTurismoV2/app/components/Slider.tsx
@@ -48,6 +48,8 @@ export default function Slider(){
             loop={true}
             lazyPreloadPrevNext={1}
             centeredSlides={true}
+            slidesPerView={1}
+            resizeObserver={false}
             >
             <SwiperSlide>
                 <SliderItem imgUrl={imagenes[0]} titulo="Descubre España con Nosotros" subtitulo="Innumerables paisajes"></SliderItem>
