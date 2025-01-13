be58148debc14a9ceed7e09b2c36f6d94e533b6f
anthony_guanga <ninfrad23@gmail.com>
2025-01-13 22:08:28 +0100

    add scrip en el fichero de provincias.provinci

--- ProyectoTurismoV2/app/routes/provincias.provincia.tsx
+++ ProyectoTurismoV2/app/routes/provincias.provincia.tsx
@@ -1,8 +1,31 @@
-export default function provincia() {
+import { PrismaClient } from "@prisma/client";
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import db from "~/db.server";
+// Define el tipo de Provincia
+type Province = {
+  id: number;
+  name: string;
+};
+
+export const loader: LoaderFunction = async () => {
+  const db = new PrismaClient();
+  const provincialstables = await db.provinces.findMany();
+  return json({ provincialstables });
+};
+
+export default function Provincia() {
+  const data = useLoaderData<{ provincialstables: Province[] }>();
+
   return (
     <div>
-      <h1>hola</h1>
+      <h1>Hola</h1>
       <p>Aquí se ve cada provincia específica</p>
+      <ul>
+        {data.provincialstables.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- ProyectoTurismoV2/prisma/seed.ts
+++ ProyectoTurismoV2/prisma/seed.ts
@@ -4,48 +4,48 @@ import { createProvinces, connectCategoriesWithProvinces } from "./provincias";
 
 const db = new PrismaClient();
 
-async function crearUser(){
-    const password="ejemplo";
-    const passwordConHash= await hashPassword(password);
+async function crearUser() {
+  const password = "ejemplo";
+  const passwordConHash = await hashPassword(password);
 
-    return db.user.create({
-        data:{
-            email:"ejemplo@gmail.com",
-            name:"Ejemplo",
-           password:passwordConHash
-        }
-    });
+  return db.user.create({
+    data: {
+      email: "ejemplo@gmail.com",
+      name: "Ejemplo",
+      password: passwordConHash,
+    },
+  });
 }
 //Función para crear un usuario de prueba.
 
-function eliminarUser(){
-    return db.user.delete({
-        where:{
-            email:"ejemplo@gmail.com"
-        }
-    });
+function eliminarUser() {
+  return db.user.delete({
+    where: {
+      email: "ejemplo@gmail.com",
+    },
+  });
 }
-function createCategory(){
-    return db.category.createMany({
-        data:[
-            {
-                name:"costa"
-            },
-            {
-                name:"islas"
-            },
-            {
-                name:"sierra"
-            }
-        ]
-    })
+function createCategory() {
+  return db.category.createMany({
+    data: [
+      {
+        name: "costa",
+      },
+      {
+        name: "islas",
+      },
+      {
+        name: "sierra",
+      },
+    ],
+  });
 }
 
 async function seed() {
-    const user=await crearUser();
-    const category= await createCategory();
-    const provinces=await createProvinces();
-    await connectCategoriesWithProvinces();
+  const user = await crearUser();
+  const category = await createCategory();
+  const provinces = await createProvinces();
+  await connectCategoriesWithProvinces();
 }
 
 seed();
