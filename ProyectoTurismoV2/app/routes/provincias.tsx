5f9c2335c4c8a8830a1dd23ee870078faa85d22a
alex <ac8369745@gmail.com>
2025-01-13 19:20:40 +0100

    Se crea el componente ProvinceCard en el cual se mostrara toda la información de cada provincia y tambien un tipo especifico para este componente

--- ProyectoTurismoV2/app/routes/provincias.tsx
+++ ProyectoTurismoV2/app/routes/provincias.tsx
@@ -1,4 +1,5 @@
 import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import classNames from "classnames";
 import { NavLink, Outlet } from "react-router-dom";
 import { getProvincies } from "~/models/provinces.server";
@@ -10,6 +11,7 @@ export const loader= async() => {
 }
 
 export default function provincias() {
+  const data= useLoaderData<typeof loader>();
   return (
     <div className="provincias">
       <h1 className="provincias__titulo">Provincias</h1>
@@ -32,7 +34,10 @@ export default function provincias() {
       <div>
             <h1 className="text-4xl text-red-600 font-semibold uppercase mt-6 text-center">Provincias de España</h1>
             <div className="my-6 mx-16 grid auto-cols-[minmax(_1fr,_2fr)] gap-2 md:grid-cols-2">
-                <p>Provincia aqui</p>
+                {data.map((provincia)=>{
+                    return <ProvincieCard imageUrl={"../../"+provincia.imageUrl} name={provincia.name} info={provincia.description} climate={provincia.climate}
+                    places={provincia.recommendedPlaces} food={provincia.typicalFood} festivities={provincia.festivities} beaches={provincia.beaches}></ProvincieCard>
+                })}
             </div>
         </div>
     </div>
@@ -43,4 +48,37 @@ export default function provincias() {
 function provincia(){
 
 }
-//Aquí se puede hacer el componente para cada provincia.
+type ProvinceCardProps={
+  imageUrl:string,    
+  name:string,
+  info:string,
+  climate:string,
+  beaches:string | null,
+  places:string,
+  food:string,
+  festivities:string,
+}
+
+function ProvincieCard({imageUrl,name,info,climate,beaches,places,food,festivities}:ProvinceCardProps){
+  const playa=false;
+  return(
+      <div className={classNames("border-2 border-red-400 rounded-md",
+          "grid grid-cols-3 grid-rows-3 object-cover overflow-hidden","card"
+      )}>
+          <img src={imageUrl} alt="xd" className={classNames("w-full object-cover h-full col-start-1 col-end-4 row-start-1 row-end-3",
+              "md:col-start-1 md:col-end-4 md:p-3 md:object-cover", "imagenxd"
+          )}/>
+          <div className={classNames("row-start-3 row-end-4 col-start-1 col-end-4",
+              "md:col-start-1 md:col-end-4 md:row-start-3 md:row-end-4 md:mx-2 md:mb-1","text-start")}>
+              <h4 className="font-bold text-xl text-center mb-1 titulo">{name}</h4>
+              <p>{info}</p>
+              <p><span className="font-bold">Clima: </span>{climate}</p>
+              <p><span className="font-bold">Lugares Recomendados: </span>{places}</p>
+              {beaches ? <p><span className="font-bold">Playas: </span>{beaches}</p>:""}
+              <p><span className="font-bold">Comida típica: </span>{food}</p>
+              <p><span className="font-bold">Festividades: </span>{festivities}</p>
+              <button className="login-container__login-form__button-exit mb-1 mt-1 hover:border-red-600 self-center">Añadir</button>
+          </div>
+      </div>
+  );
+}
