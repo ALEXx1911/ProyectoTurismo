ac497039e5b3c7fde7fb370dbf7a64b090275f41
anthony_guanga <ninfrad23@gmail.com>
2025-01-14 21:08:30 +0100

    add nuevas lineas de codigo en el fichero de provincias

--- ProyectoTurismoV2/app/routes/provincias.provincia.tsx
+++ ProyectoTurismoV2/app/routes/provincias.provincia.tsx
@@ -1,11 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import classNames from "classnames";
 import db from "~/db.server";
+import { getProvincies } from "~/models/provinces.server";
 // Define el tipo de Provincia
 type Province = {
   id: number;
   name: string;
+  description: String;
 };
 
 export const loader: LoaderFunction = async () => {
@@ -19,11 +22,23 @@ export default function Provincia() {
 
   return (
     <div>
-      <h1>Hola</h1>
-      <p>Aquí se ve cada provincia específica</p>
-      <ul>
-        {data.provincialstables.map((user) => (
-          <li key={user.id}>{user.name}</li>
+      <ul
+        className={classNames(
+          "flex gap-8 overflow-x-auto",
+          "snap-x snap-mandatory"
+        )}
+      >
+        {data.provincialstables.map((province) => (
+          <li
+            key={province.id}
+            className={classNames(
+              "border-2 border-red-500 rounded-md p4",
+              "w-[calc(100vw-2rem)] flex-none snap-center"
+            )}
+          >
+            <h1 className="text-2xl font-extrabold">{province.name}</h1>
+            <p>{province.description}</p> {}
+          </li>
         ))}
       </ul>
     </div>
