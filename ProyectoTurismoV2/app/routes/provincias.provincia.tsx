344c3294e77764328e5be7462a22e35ea636231c
anthony_guanga <ninfrad23@gmail.com>
2025-01-15 20:49:39 +0100

    add funciones para la navegacion sin javacrips

--- ProyectoTurismoV2/app/routes/provincias.provincia.tsx
+++ ProyectoTurismoV2/app/routes/provincias.provincia.tsx
@@ -1,46 +1,63 @@
 import { PrismaClient } from "@prisma/client";
-import { type LoaderArgs,json} from "@remix-run/node";
+import { type LoaderArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import classNames from "classnames";
 import db from "~/db.server";
 import { getAllProvincies } from "~/models/provinces.server";
-import {SearchIcon} from "~/components/icons";
-import { LoaderFunction, LoaderFunctionArgs, useSearchParams } from "react-router-dom";
+import { SearchIcon } from "~/components/icons";
+import {
+  Form,
+  LoaderFunction,
+  LoaderFunctionArgs,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
+import { Navigation } from "swiper/modules";
 // Define el tipo de Provincia
 type Province = {
   id: number;
   name: string;
   description: String;
 };
-type loaderData={
+type loaderData = {
   provincialstables: Awaited<ReturnType<typeof getAllProvincies>>;
-}
-export const loader: LoaderFunction = async ({ request }) =>{
+};
+export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
-  const db = new PrismaClient();
   const provincialstables = await getAllProvincies(q);
   return json({ provincialstables });
 };
 
 export default function Provincia() {
   const data = useLoaderData() as loaderData;
-const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const navigation = useNavigation();
+  const isSearching = navigation.formData?.has("q");
   return (
     <div>
-      <form className={classNames("flex border-2 border-gray-300 rounded-md" ,
-        "focus-within:border-red-500 md:w-80")}>
+      <Form
+        className={classNames(
+          "flex border-2 border-gray-300 rounded-md",
+          "focus-within:border-red-500 md:w-80",
+          isSearching ? "animate-pulse" : ""
+        )}
+      >
         <button className="px-2 mr-1">
-          <SearchIcon/>
+          <SearchIcon />
         </button>
         <input
-        type="text"
-        name="q"
-        autoComplete="off"
-        placeholder="Search"
-        className="w-full py-3 px-2 outline-none"
+          defaultValue={searchParams.get("q") ?? ""}
+          type="text"
+          name="q"
+          autoComplete="off"
+          placeholder="Search"
+          className="w-full py-3 px-2 outline-none"
         />
-      </form>
+      </Form>
+      <Form reloadDocument>
+        <button></button>
+      </Form>
       <ul
         className={classNames(
           "flex gap-8 overflow-x-auto mt-4",
