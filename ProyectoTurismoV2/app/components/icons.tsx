902d13cc6638a1e001c5fcb195e902526b692cea
alex <ac8369745@gmail.com>
2025-01-27 22:59:56 +0100

    Se añade los iconos de las flechas y se crea una función con la que se crean y modifican los searchParams para poder paginar las provincias

--- ProyectoTurismoV2/app/components/icons.tsx
+++ ProyectoTurismoV2/app/components/icons.tsx
@@ -88,3 +88,21 @@ export function Heart() {
   );
 }
 //Icono de corazón relleno.
+export function RightArrow(){
+  return(
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+      </svg>
+    </>
+  );
+}
+export function LeftArrow(){
+  return(
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+      </svg>
+    </>
+  );
+}
