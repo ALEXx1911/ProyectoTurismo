3f371b24926906e6db676b2d5ddcc5937753111f
anthony_guanga <ninfrad23@gmail.com>
2025-03-03 13:34:08 +0100

    add se elimino la mini barra de busqueda de itinerario

--- ProyectoTurismoV2/app/routes/itinerarioDeViajes.tsx
+++ ProyectoTurismoV2/app/routes/itinerarioDeViajes.tsx
@@ -119,26 +119,6 @@ export default function ItinerarioDeViajes() {
         ITINERARIO DE VIAJES
       </h1>
 
-      <Form
-        className={classNames(
-          "flex border-2 border-gray-300 rounded-md",
-          "focus-within:border-red-500 md:w-80",
-          isSearching ? "animate-pulse" : ""
-        )}
-      >
-        <button className="px-2 mr-1">
-          <SearchIcon />
-        </button>
-        <input
-          defaultValue={searchParams.get("q") ?? ""}
-          type="text"
-          name="q"
-          autoComplete="off"
-          placeholder="Buscar"
-          className="w-full py-3 px-2 outline-none"
-        />
-      </Form>
-
       <createItinerarioFetcher.Form method="post">
         <PrimaryButton
           name="_action"
